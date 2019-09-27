6ab8d398063bb5e1c76d91bd361f6f8c71fbc440
TomRangren <54435262+TomRangren@users.noreply.github.com>
2019-09-27 09:37:44 +0200

    Create Inluppg3.cs

---
+++ Inluppg3.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 10, b = 20;
+            BytPlats(ref a, ref b);
+            Console.WriteLine("a= " + a +" "+"b=" + b);
+        }
+            public static void BytPlats(ref int c, ref int d) {
+            int temp = c;
+            c = d;
+            d = temp;
+        }
+    }
+}
